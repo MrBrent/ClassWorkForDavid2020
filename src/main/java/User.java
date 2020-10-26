7949e8b51b1c18124950f1c500341960bc5466dd
Administrator <nobody@nobody.com>
2020-10-26 17:48:44 +0000

    Users can register and log in

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -4,6 +4,7 @@ public class User {
     private final String email;
     private final String username;
     private final String password;
+    private boolean loggedIn = false;
 
     public User(String firstName, String lastName, String email, String username, String password) {
         this.firstName = firstName;
@@ -31,5 +32,17 @@ public class User {
 
     public String getPassword() {
         return password;
+    }
+
+    public boolean isLoggedIn() {
+        return loggedIn;
+    }
+
+    public boolean verifyPassword(String password) {
+        return this.password.equals(password);
+    }
+
+    public void logIn() {
+        loggedIn = true;
     }
 }
--- src/test/java/TestUser.java
+++ src/test/java/TestUser.java
@@ -3,6 +3,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestUser {
+
     @Test
     void testUser() {
         String firstName = "Jacob";
@@ -18,5 +19,28 @@ public class TestUser {
         assertEquals(email, uut.getEmail());
         assertEquals(username, uut.getUsername());
         assertEquals(password, uut.getPassword());
+    }
+
+    @Test
+    void testRegisterUser() {
+//        User created: Jacob Nash
+
+        String firstName = "Jacob";
+        String lastName = "Nash";
+        String email = "Jacob.Nash@intel.com";
+        String username = "JNashty";
+        String password = "12345";
+
+        User uut = new User(firstName, lastName, email, username, password);
+
+        Users users = new Users();
+        //        Register Jacob Nash
+        users.register(uut);
+        //        Verify that he is not logged in
+        assertEquals(false, uut.isLoggedIn());
+        //        Log Jacob Nash in with password and username
+        User somebodyWhoLoggedIn = users.logIn("JNashty", "12345");
+        //        Verify that he is logged
+        assertEquals(true, somebodyWhoLoggedIn.isLoggedIn());
     }
 }
